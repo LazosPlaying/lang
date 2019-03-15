aedc783499ef66da5699989f824ceb8b8438aff7
aljaxus <aljaxus.dev@gmail.com>
2019-03-15 16:44:29 +0100

    Update and rename 2018_07_29-replace-urls-with-html_a.js to replace_url_in_string_to_anchors.txt

--- javascript/2018_07_29-replace-urls-with-html_a.js
+++
@@ -1,4 +0,0 @@
-let string = 'some string with google.com address and https://dev.aljaxus.eu URL';
-let exp = /((https?:\/\/)?([a-z0-9\.\-]*)\.([a-z0-9]{2,9})((\s)|((\/|\?)([a-z0-9\/<>!\?\=\&]*))))/ig;
-
-let stringWithLinks = string.replace(exp, '<a href="$1">$2$3.$4</a>');
